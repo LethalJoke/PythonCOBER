8d57dbdb0084355577c9e1263cc399841294598c
adrien <pedeau.adrien@gmail.com>
2022-05-10 11:55:59 +0200

    edited list test to cover more cases

--- tests/python_cober_tests.py
+++ tests/python_cober_tests.py
@@ -16,10 +16,11 @@ class PythonCoberTests(unittest.TestCase):
     def test_when_number_is_not_divisible_by_three_and_five_should_return_cober(self):
         self.assertEqual(cober_from_number_to_string(15), 'cober', "Should be 'cober'")
 
-    def test_when_list_only_contains_non_multipliers_of_three_and_five_should_return_number_list(self):
-        input_list = [1, 2, 4, 7, 13]
-        expected_output = ['1', '2', '4', '7', '13']
+    def test_when_list_used_on_cober_function_should_return_proper_output(self):
+        input_list = [1, 3, 5, 15, 13]
+        expected_output = ['1', 'co', 'ber', 'cober', '13']
         self.assertEqual(cober_from_number_list_to_string_list(input_list), expected_output, "Should be " + str(expected_output))
+
 
 if __name__ == '__main__':
     unittest.main()
